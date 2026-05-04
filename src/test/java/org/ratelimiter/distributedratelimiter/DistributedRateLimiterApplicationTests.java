05ec6f5a66ece2cce05ee3e3c15ef6821d577769
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2026-05-05 01:49:13 +0530

    Initial commit

---
+++ src/test/java/org/ratelimiter/distributedratelimiter/DistributedRateLimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ratelimiter.distributedratelimiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedRateLimiterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
