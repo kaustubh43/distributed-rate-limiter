2f0938a83fdf52b74528428951e9e0dfebaa3fd2
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2026-05-05 17:48:21 +0530

    Add Redis-based rate limiting functionality and configuration
    - Introduced rate limiting Lua script for token bucket algorithm
    - Implemented RateLimitService to manage rate limiting logic
    - Configured Redis connection and script execution in RedisConfig
    - Updated application properties to set server port
    - Added Redis dependency in pom.xml

---
+++ src/main/java/org/ratelimiter/distributedratelimiter/configurations/RedisConfig.java
@@ -0,0 +1,34 @@
+package org.ratelimiter.distributedratelimiter.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisScript<Long> rateLimitLuaScript() {
+        DefaultRedisScript<Long> redisScript = new DefaultRedisScript<>();
+        // Points to src/main/resources/rate_limiter.lua
+        redisScript.setLocation(new ClassPathResource("rate_limiter.lua"));
+        // Lua returns 1 or 0, which Spring maps to a Long
+        redisScript.setResultType(Long.class);
+        return redisScript;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(RedisSerializer.string());
+        redisTemplate.setHashKeySerializer(RedisSerializer.string());
+        redisTemplate.setValueSerializer(RedisSerializer.json());
+        redisTemplate.setHashValueSerializer(RedisSerializer.json());
+        return redisTemplate;
+    }
+}
---
+++ src/main/java/org/ratelimiter/distributedratelimiter/services/RateLimitService.java
@@ -0,0 +1,43 @@
+package org.ratelimiter.distributedratelimiter.services;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.RedisScript;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class RateLimitService {
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final RedisScript<Long> rateLimitLuaScript;
+
+    private static final String BUCKET_CAPACITY = "15";
+    private static final String BUCKET_REFILL_RATE_PER_SEC = "5";
+    private static final String REDIS_RATE_LIMIT_KEY = "rate_limit:client:";
+
+    public RateLimitService(RedisTemplate<String, Object> redisTemplate, RedisScript<Long> rateLimitLuaScript) {
+        this.redisTemplate = redisTemplate;
+        this.rateLimitLuaScript = rateLimitLuaScript;
+    }
+
+    public boolean isAllowed(String clientId) {
+        if(clientId == null) {
+            return false;
+        }
+        List<String> keys = Collections.singletonList(REDIS_RATE_LIMIT_KEY +  clientId);
+        String currentUnixTime = String.valueOf(Instant.now().getEpochSecond());
+
+        // Execute the atomic Lua script in Redis
+        Long result = redisTemplate.execute(rateLimitLuaScript,
+                keys,
+                BUCKET_CAPACITY,
+                BUCKET_REFILL_RATE_PER_SEC,
+                currentUnixTime
+        );
+
+        // 1 means script is executed successfully and token is decremented, 0 means bucket empty.
+        return result != null && result == 1L;
+    }
+}
